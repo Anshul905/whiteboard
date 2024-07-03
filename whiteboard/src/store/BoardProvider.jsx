9c18de831dd47eaf9668542364c99f6b4dc3d21a
Anshul Kumar Neekhara <anshulneekhara@gmail.com>
2024-07-03 21:12:35 +0530

    using useReducer

--- whiteboard/src/store/BoardProvider.jsx
+++ whiteboard/src/store/BoardProvider.jsx
@@ -1,23 +1,43 @@
-import React from 'react'
+import React, { act } from 'react'
 import boardContext from './board-context'
 import { TOOL_ITEMS } from '../constants'
-import { useState } from 'react'
+import { useReducer } from 'react'
+
+
+const boardReducer = ( state , action ) => {
+    switch (action.type) {
+        case BOARD_ACTIONS.CHANGE_TOOL:
+            return {
+                ...state ,
+                activeToolItem : action.payload.tool_item ,
+            }
+        default:
+            return state;
+    }
+}
 
 const BoardProvider = ( {children} ) => {
     
-    const [activeToolItem, setactiveToolItem] = useState(TOOL_ITEMS.RECTANGLE)
-    const [ elements , setElements] = useState(second)
+    const initialBoardState = {
+        activeToolItem : TOOL_ITEMS.LINE , 
+        elements : [] ,
+    }
+    const [ boardState, dispatchBoardAction ] = useReducer( boardReducer , initialBoardState )
 
-    const handleToolItemClick = (item) =>{
-        setactiveToolItem(item);
+    const handleToolItemClick = (tool_item) =>{
+        dispatchBoardAction( {
+            type : BOARD_ACTIONS.CHANGE_TOOL , 
+            payload : {
+                tool_item
+            } ,
+        } )
     }
 
-    
+
     const boardContextValue = {
-        activeToolItem ,
+        activeToolItem : boardState.activeToolItem,
         handleToolItemClick,    
     }
-
     return (
     <>
         <boardContext.Provider  value={boardContextValue} >
