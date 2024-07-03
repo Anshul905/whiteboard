e61b1154b1521463d5c3a36fe08c3b8994b5912d
Anshul Kumar Neekhara <anshulneekhara@gmail.com>
2024-07-03 18:55:33 +0530

    Added constants

--- whiteboard/src/components/Toolbar/index.jsx
+++ whiteboard/src/components/Toolbar/index.jsx
@@ -1,24 +1,26 @@
 import { useState , React } from 'react'
 import classes from "./index.module.css"
 
+import { TOOL_ITEMS } from '../../constants';
+
 import cx from "classnames"
 import { FaSlash } from "react-icons/fa";
 import { LuRectangleHorizontal } from "react-icons/lu";
 
 const Toolbar = () => {
-  const [activeToolItem, setactiveToolItem] = useState("LINE")
+  const [activeToolItem, setactiveToolItem] = useState(TOOL_ITEMS.LINE)
   return (
     <>
         <div className={classes.container}>
             
-            <div className={ cx(classes.toolItem , { [classes.active] : activeToolItem==="LINE" }) }
-                onClick={ () => setactiveToolItem("LINE") }
+            <div className={ cx(classes.toolItem , { [classes.active] : activeToolItem===TOOL_ITEMS.LINE }) }
+                onClick={ () => setactiveToolItem(TOOL_ITEMS.LINE) }
             >
               <FaSlash />
             </div>
 
-            <div className={ cx(classes.toolItem , { [classes.active] : activeToolItem==="RECTANGLE" }) }
-                onClick={ () => setactiveToolItem("RECTANGLE") }
+            <div className={ cx(classes.toolItem , { [classes.active] : activeToolItem===TOOL_ITEMS.RECTANGLE }) }
+                onClick={ () => setactiveToolItem(TOOL_ITEMS.RECTANGLE) }
             >
               <LuRectangleHorizontal/>
             </div>
---
+++ whiteboard/src/constants.js
@@ -0,0 +1,64 @@
+export const TOOL_ITEMS = {
+    BRUSH: "BRUSH",
+    LINE: "LINE",
+    RECTANGLE: "RECTANGLE",
+    CIRCLE: "CIRCLE",
+    ARROW: "ARROW",
+    ERASER: "ERASER",
+    TEXT: "TEXT",
+  };
+  
+  export const TOOL_ACTION_TYPES = {
+    NONE: "NONE",
+    DRAWING: "DRAWING",
+    ERASING: "ERASING",
+    WRITING: "WRITING",
+  };
+  
+  export const BOARD_ACTIONS = {
+    CHANGE_TOOL: "CHANGE_TOOL",
+    DRAW_DOWN: "DRAW_DOWN",
+    DRAW_MOVE: "DRAW_MOVE",
+    DRAW_UP: "DRAW_UP",
+    ERASE: "ERASE",
+    CHANGE_ACTION_TYPE: "CHANGE_ACTION_TYPE",
+    CHANGE_TEXT: "CHANGE_TEXT",
+    UNDO: "UNDO",
+    REDO: "REDO",
+  };
+  
+  export const COLORS = {
+    BLACK: "#000000",
+    RED: "#ff0000",
+    GREEN: "#00ff00",
+    BLUE: "#0000ff",
+    ORANGE: "#ffa500",
+    YELLOW: "#ffff00",
+    WHITE: "#ffffff",
+  };
+  
+  export const TOOLBOX_ACTIONS = {
+    CHANGE_STROKE: "CHANGE_STROKE",
+    CHANGE_FILL: "CHANGE_FILL",
+    CHANGE_SIZE: "CHANGE_SIZE",
+  };
+  
+  export const FILL_TOOL_TYPES = [TOOL_ITEMS.RECTANGLE, TOOL_ITEMS.CIRCLE];
+  export const STROKE_TOOL_TYPES = [
+    TOOL_ITEMS.BRUSH,
+    TOOL_ITEMS.LINE,
+    TOOL_ITEMS.ARROW,
+    TOOL_ITEMS.RECTANGLE,
+    TOOL_ITEMS.CIRCLE,
+    TOOL_ITEMS.TEXT,
+  ];
+  export const SIZE_TOOL_TYPES = [
+    TOOL_ITEMS.LINE,
+    TOOL_ITEMS.ARROW,
+    TOOL_ITEMS.RECTANGLE,
+    TOOL_ITEMS.CIRCLE,
+    TOOL_ITEMS.TEXT,
+  ];
+  
+  export const ARROW_LENGTH = 20;
+  export const ELEMENT_ERASE_THRESHOLD = 0.1;
