2ee5c5456288617c4538a58e9ea6945d593574a3
Anshul Kumar Neekhara <anshulneekhara@gmail.com>
2024-08-04 13:15:33 +0530

    Download feature is done

--- whiteboard/src/components/Toolbar/index.jsx
+++ whiteboard/src/components/Toolbar/index.jsx
@@ -5,7 +5,7 @@ import cx from "classnames"
 
 import { TOOL_ITEMS } from '../../constants';
 
-import { FaSlash , FaRegCircle , FaArrowRight , FaPaintBrush , FaEraser , FaFont , FaUndoAlt , FaRedoAlt } from "react-icons/fa";
+import { FaSlash , FaRegCircle , FaArrowRight , FaPaintBrush , FaEraser , FaFont , FaUndoAlt , FaRedoAlt , FaDownload} from "react-icons/fa";
 import { LuRectangleHorizontal } from "react-icons/lu";
 
 import boardContext from '../../store/board-context';
@@ -15,6 +15,16 @@ import { useContext } from 'react';
 const Toolbar = () => {
 
   const { activeToolItem , changeToolHandler , undo , redo } = useContext(boardContext)
+
+  const handleDownloadClick = () => {
+    const canvas = document.getElementById("canvas");
+    const data = canvas.toDataURL("image/png") ;
+    const anchor = document.createElement("a"); 
+    anchor.href = data;
+    anchor.download = "board.png";
+    anchor.click();
+    
+  }
 
   return (
     <>
@@ -63,19 +73,20 @@ const Toolbar = () => {
               <FaFont/>
             </div>
             
-            <div className={ classes.toolItem }
-                onClick={ undo }
-            >
+
+            <div className={ classes.toolItem } onClick={ undo }>
               <FaUndoAlt/>
             </div>
 
 
-            <div className={ classes.toolItem }
-                onClick={ redo }
-            >
+            <div className={ classes.toolItem } onClick={ redo }>
               <FaRedoAlt/>
             </div>
 
+
+            <div className={ classes.toolItem } onClick={ handleDownloadClick }>
+              <FaDownload/>
+            </div>
 
 
         </div>    
