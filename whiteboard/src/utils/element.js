4d712eb8e9c1eaba2a31c671f6b78b02d1703787
Anshul Kumar Neekhara <anshulneekhara@gmail.com>
2024-08-04 12:57:22 +0530

    Erasing Text is fixed

--- whiteboard/src/utils/element.js
+++ whiteboard/src/utils/element.js
@@ -109,9 +109,38 @@ export const isCusrorNearElement = (element, pointX, pointY) => {
             isPointCloseToLine(x2, y2, x1, y2, pointX, pointY) ||
             isPointCloseToLine(x1, y2, x1, y1, pointX, pointY)
             );        
-        case TOOL_ITEMS.BRUSH:
+        case TOOL_ITEMS.BRUSH:{
             const context = document.getElementById("canvas").getContext("2d");
             return context.isPointInPath(element.path, pointX, pointY);
+        }
+        case TOOL_ITEMS.TEXT:{
+            const context = document.getElementById("canvas").getContext("2d");
+            context.font = `${element.size}px Caveat`;
+            context.fillStyle = element.stroke;
+            const textWidth = context.measureText(element.text).width;
+            const textHeight = parseInt(element.size);
+            context.restore();
+            return (
+                isPointCloseToLine(x1, y1, x1 + textWidth, y1, pointX, pointY) ||
+                isPointCloseToLine(
+                x1 + textWidth,
+                y1,
+                x1 + textWidth,
+                y1 + textHeight,
+                pointX,
+                pointY
+                ) ||
+                isPointCloseToLine(
+                x1 + textWidth,
+                y1 + textHeight,
+                x1,
+                y1 + textHeight,
+                pointX,
+                pointY
+                ) ||
+                isPointCloseToLine(x1, y1 + textHeight, x1, y1, pointX, pointY)
+            );  
+        }                     
         default:
             throw new Error("Type not recognized");
             return false;
